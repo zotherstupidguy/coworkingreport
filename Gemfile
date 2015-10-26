6d18654002951ef971625961729c87f99b91918c
zotherstupidguy <zotherstupidguy@gmail.com>
2015-10-26 17:09:40 +0200

    now index pages gets only upcoming events

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'shotgun'
+gem 'puma'
 gem 'redis'
--- app.rb
+++ app.rb
@@ -8,23 +8,40 @@ get '/' do
   @upcoming_events 	= []
 
   @spaces.each do |space|
-    @data 	= Marshal.load(@r.get(space))
+    @space_events 	= Marshal.load(@r.get(space))
 
-    @data.each do |event|
-      p event
+    @space_events.each do |event|
+      $logger.info("#{event} is on iteration")
+
 
       if !event["start_time"].nil? then 
+
 	@event_date = Time.parse(event["start_time"])
-	@event_date > Time.now ?  @coming_up = true : @coming_up = false unless @event_date.nil? 
-	if @coming_up then 
+
+	#@event_date > Time.parse(Time.now.to_s) ?  @coming_up = true : @coming_up = false # unless @event_date.nil? 
+
+	#if @coming_up then 
+	if @event_date > Time.parse(Time.now.to_s) 
+
+	  $logger.info("##############")
+	  $logger.info("##############")
+	  $logger.info("##############")
+	  $logger.info("##############")
+	  $logger.info("we got an upcoming on #{@event_date} and today is #{Time.parse(Time.now.to_s)}")
+	  $logger.info("##############")
+	  $logger.info("##############")
+	  $logger.info("##############")
+	  $logger.info("##############")
+
 	  @upcoming_events << event
+	  $logger.info("############### Adding #{event} to @upcoming_events ##############")
 	end
       end
     end
-  
+
   end
-  p "start our logging"
-  p @upcoming_events
+  #p "start our logging"
+  #p @upcoming_events
   erb :index
 end
 
@@ -34,8 +51,8 @@ get '/:space' do
     fetch_events(params["space"])
   end
   @space	= params["space"]
-  @data 	= Marshal.load(@r.get(params[:space]))
-  @total 	= @data.count
+  @space_events 	= Marshal.load(@r.get(params[:space]))
+  @total 	= @space_events.count
 
   erb :space
 end
